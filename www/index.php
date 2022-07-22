50565110ddb18726dd8304891b3e6a3c1f3764b0
Kevin97125 <108547007+Kevin97125@users.noreply.github.com>
2022-07-22 12:21:27 -0400

    recuperation de la derniere version.

--- www/index.php
+++ www/index.php
@@ -1,69 +1,84 @@
-<?php 
-define("IS_DEBUG", $_SERVER["HTTP_HOST"] == "localhost" ? true : false);
+<?php
+    
+    define("IS_DEBUG", $_SERVER["HTTP_HOST"] == "localhost" ? true : false);
 
-$firstname = $lastname = $subject = $email = $message = ""; //initialise la variable a 0
-$firstnameError = $lastnameError = $subjectError = $emailError = $messageError = ""; 
-$noError = true;
+    $firstname = $lastname = $subject = $email = $message = "";
+    $firstnameError = $lastnameError = $subjectError = $emailError = $messageError = "";
+    
+    if($_SERVER["REQUEST_METHOD"] == "POST"){
+        if(IS_DEBUG){
+            echo "POST<br>";
+        }
+        $noError = true;
+        $emailTo = "nvformateurdwdm@gmail.com";
+        $emailText = "";
+        // firstname
+        $firstname = isset($_POST["firstname"]) ? checkInput($_POST["firstname"]) : "";
 
-//Les variables PHP $_GET et $_POST sont utilisées pour collectées les données postées par les formulaires. $_GET et $_POST sont des tableaux associatifs contenant des paires clé/valeur où les clés sont les noms des contrôles de formulaires (name="...") et les valeurs sont les données d'entrée de l'utilisateur.
+        if(empty($firstname)){
+            $firstnameError = "Veuillez renseigner votre prénom.";
+        }else{
+            $emailText .= "Prénom : " . $firstname . "\n";
+        }
+        // lastname
+        $lastname = isset($_POST["lastname"]) ? checkInput($_POST["lastname"]) : "";
+        if(empty($lastname)){
+            $lastnameError = "Veuillez renseigner votre nom.";
+        }else{
+            $emailText .= "Nom : " . $lastname . "\n";
+        }
+        //
+        $subject = isset($_POST["subject"]) ? checkInput($_POST["subject"]) : "";
+        if(empty($subject)){
+            $subjectError = "Veuillez renseigner le sujet.";
+        }
+        //
+        $email = isset($_POST["email"]) ? checkInput($_POST["email"]) : "";
+        if(!isEmail($email)){
+            $emailError = "Veuillez vérifier votre email.";
+        }else{
+            $emailText .= "email : " . $email . "\n";
+        }
+        //
+        $message = isset($_POST["message"]) ? checkInput($_POST["message"]) : "";
+        if(empty($message)){
+            $messageError = "Veuillez taper votre message.";
+        }else{
+            $emailText .= "Message : " . $message . "\n";
+        }
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") { //$_SERVER : ce sont des valeurs utiles que nous donne le serveur. Pour les afficher, il faut indiquer ce qu'on demande entre crochets puisque que c'est un array.
-    if(IS_DEBUG){
-        echo "POST";
+        $noError = $firstnameError == "" && $lastnameError == "" && $subjectError == "" && $emailError == "" && $messageError == "";
+
+        if($noError){
+            $headers = "From: $firstname $lastname <$email>\r\nReply-To: $email";
+            mail($emailTo, $subject, $emailText, $headers);
+        }
+
+    }else{
+        if(IS_DEBUG){
+            echo "Pas POST";
+        }
     }
-    $firstname = isset($_POST["firstname"]) ? checkInput($_POST["firstname"]) : ""; //La fonction isset() est une fonction intégrée en PHP qui vérifie si une variable est définie, ce qui signifie qu’elle doit être déclarée et non NULL. Cette fonction renvoie TRUE si la variable existe et n’est pas NULL, sinon elle renvoie FALSE.
-    if (empty($firtsname)) {
-        $firstnameError = "Veuillez renseigner votre Prenom.";
-        $noError = false;
+
+    function checkInput($input){
+        $input = trim($input);
+        $input = stripslashes($input);
+        $input = htmlspecialchars($input);
+        if(IS_DEBUG){
+            echo $input;
+            echo "<br>";
+        }
+        return $input;
     }
 
-    $lastname = isset($_POST["lastname"]) ? checkInput($_POST["lastname"]) : "";
-    if (empty($lastname)) {
-        $lastnameError = "Veuillez renseigner votre Nom.";
-        $noError = false;
+    function isEmail($email){
+        return filter_var($email, FILTER_VALIDATE_EMAIL);
     }
 
-    $subject = isset($_POST["subject"]) ? checkInput($_POST["subject"]) : "";
-    if (empty($subject)) {
-        $subject = "Veuillez renseigner le sujet.";
-        $noError = false;
+    function getError($error){
+        $html = '<h2 class="error">' . $error .'</h2>'; 
+        return $html; 
     }
-
-    $email = isset($_POST["email"]) ? checkInput($_POST["email"]) : "";
-    if (!isEmail($email)) {//condition le message qui est contenu dans $emailError si l'email est pas bon.
-        $emailError = "Veuillez vérifier votre email.";
-    }
-
-    $message = isset($_POST["message"]) ? checkInput($_POST["message"]) : "";
-    if (empty($message)) {
-        $messageError = "Veuillez tapez votre message.";
-        $noError = false;
-    }
-} else {
-    if (IS_DEBUG) {
-        echo "Pas de POST";
-    }
-}
-
-function checkInput($input){ //fonction qui vérifie les Input
-    $input = trim($input); //retourne la chaîne str, après avoir supprimé les caractères invisibles en début et fin de chaîne. Si le second paramètre charlist est omis, trim() supprimera les caractères suivants :
-    $input = stripslashes($input); //Supprime les antislashs d'une chaîne
-    $input = htmlspecialchars($input); //Convertit les caractères spéciaux en entités HTML
-    if (IS_DEBUG) {
-        echo $input;
-        echo "<br>";
-    }
-    return $input;
-}
-
-function isEmail($email){// fonction qui verifie l'email
-    return filter_var($email, FILTER_VALIDATE_EMAIL);
-}
-
-function getError($error){
-    $html = '<p class="error">' . $error . '</p>';
-    return $html;
-}
 ?>
 
 <!doctype html>
@@ -77,45 +92,41 @@ function getError($error){
 </head>
 
 <body>
-    <div id="formulaire">
-        <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"])?>">
-            <!-- les variable sont appeler par l'attribut name -->
-            <!-- //value permet de afficher ce que l'utilisateur a tapez -->
 
-            <input type="text" placeholder="Prenom" name="firstname" value="<?php echo $firstname ?>"
-                <?php echo $firstname ?> <?php echo !IS_DEBUG ? "required" : "" ?>>
-            <?php 
-                if ($firstnameError != "") {
+    <div id="formulaire"> 
+        <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]) ?>">
+            <input type="text" placeholder="Prénom" name="firstname" value="<?php echo $firstname ?>" <?php echo !IS_DEBUG ? "required" : "" ?> >
+            <?php
+                if($firstnameError != ""){
                     echo getError($firstnameError);
                 }
             ?>
-
-            <input type="text" placeholder="Nom" name="lastname" value="<?php echo $lastname ?>" <?php echo $lastname ?>
-                <?php echo !IS_DEBUG ? "required" : "" ?>>
-            <?php 
-                if ($lastnameError != "") {
-                    echo getError($lastnameError); 
+            <input type="text" placeholder="Nom" name="lastname" value="<?php echo $lastname ?>" <?php echo !IS_DEBUG ? "required" : "" ?> >
+            <?php
+                if($lastnameError != ""){
+                    echo getError($lastnameError);
                 }
             ?>
-
-            <input type="text" placeholder="Sujet" name="subject" value="<?php echo $subject ?>" <?php echo $subject ?>
-                <?php echo !IS_DEBUG ? "required" : "" ?>>
-            <?php 
-                if ($subjectError!= "") {
+            <input type="text" placeholder="Sujet" name="subject" value="<?php echo $subject ?>" <?php echo !IS_DEBUG ? "required" : "" ?> >
+            <?php
+                if($subjectError != ""){
                     echo getError($subjectError);
                 }
             ?>
-
-            <input type="email" placeholder="exemple@gmail.com" name="email" value="<?php echo $email ?>"
-                <?php echo !IS_DEBUG ? "required" : "" ?>>
+            <input type="email" placeholder="exemple@email.com" name="email" value="<?php echo $email ?>" <?php echo !IS_DEBUG ? "required" : "" ?> >
             <?php 
-                if ($emailError != "") {
-                    echo getError($emailError); //permet d'afficher le message erreur contenu dans $emailError
+                if($emailError != ""){
+                    echo getError($emailError);   
                 }
             ?>
-
-            <textarea name="message" placeholder="Tapez votre meessage" cols="30" rows="10" require>
-            <?php echo $message ?> </textarea>
+            <!-- <p class="error">Veuillez vérifier votre email.</p> -->
+            <textarea cols="30" placeholder="Tapez votre message." rows="10" name="message" <?php echo !IS_DEBUG ? "required" : "" ?> ><?php echo $message ?></textarea>
+            <?php
+                if($messageError != ""){
+                    echo getError($messageError);
+                }
+            ?>
+            <!-- <input type="password" placeholder="mot de passe" required> -->
             <!-- <div id="select"> 
             <select name="date">
                 <option>1</option>
@@ -140,8 +151,7 @@ function getError($error){
             </select>
             </div> -->
             <input type="submit" value="ENVOYER">
+            <p class="message" style="display: <?php echo (isset($noError) && $noError) ? "block" : "none"; ?>" >Message envoyé !</p>
         </form>
     </div>
-</body>
-
-</html>
+</body></html>
